39ec2d8d08a539ba7c7d7fc8a06a1ca8bb25db6c
AlbertWebs <albertmuhatia@gmail.com>
2025-04-17 09:15:52 +0300

    update google code

--- resources/views/front/google.blade.php
+++ resources/views/front/google.blade.php
@@ -1,4 +1,4 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
+{{-- <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141449503-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -17,4 +17,15 @@
   gtag('js', new Date());
 
   gtag('config', 'GT-WRGJL7VG');
+</script> --}}
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0HFHVJ955Q"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0HFHVJ955Q');
 </script>
